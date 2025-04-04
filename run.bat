d54441c612df0f932ea0254e53c835e5466b13d4
Carlos Magno Oliveira de Abreu <magno.mabreu@gmail.com>
2025-04-03 21:41:58 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 # docker build --tag novo-geoportal .
 # docker run --name novo-geoportal -v C:/magno/novo-geoportal:/usr/src/app -p 4200:4200 -it novo-geoportal
 # docker exec -it novo-geoportal /bin/sh
-
+# npm install -g npm@11.2.0
 # npm install -g @angular/cli@latest
 
 # ng serve --host 0.0.0.0 --configuration development --poll=2000 --disable-host-check
